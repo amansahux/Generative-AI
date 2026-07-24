b37341cc3a322d3fdb37f578c2b1889067785b4c
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-07-24 13:57:43 +0530

    feat: implement Pinecone vector retrieval logic and mock query response data

--- Rag Revision/main.js
+++ Rag Revision/main.js
@@ -10,17 +10,17 @@ import { Pinecone } from "@pinecone-database/pinecone";
 const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
 const index = pc.index("rag-revision");
 
-const buffer = fs.readFileSync("story.pdf");
-const parser = new PDFParse({ data: buffer });
-const document = await parser.getText();
-// console.log(document.text)
-const text = String(document.text);
+// const buffer = fs.readFileSync("story.pdf");
+// const parser = new PDFParse({ data: buffer });
+// const document = await parser.getText();
+// // console.log(document.text)
+// const text = String(document.text);
 
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 0,
-});
-const chunks = await splitter.splitText(text);
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 500,
+//   chunkOverlap: 0,
+// });
+// const chunks = await splitter.splitText(text);
 
 // console.log(chunks);
 // console.log(chunks.length);
@@ -30,25 +30,35 @@ const embeddings = new MistralAIEmbeddings({
   apiKey: process.env.MISTRAL_API_KEY,
 });
 
-const docs = await Promise.all(
-  chunks.map(async (chunk) => {
-    const embedding = await embeddings.embedQuery(chunk);
-    return {
-      text: chunk,
-      embedding,
-    };
-  }),
-);
+// const docs = await Promise.all(
+//   chunks.map(async (chunk) => {
+//     const embedding = await embeddings.embedQuery(chunk);
+//     return {
+//       text: chunk,
+//       embedding,
+//     };
+//   }),
+// );
 // console.log(docs);
 // console.log(docs.length)
 
-await index.upsert(
-  docs.map((doc, i) => ({
-    id: `doc-${i}`,
-    values: doc.embedding,
-    metadata: {
-      text: doc.text,
-    },
-  })),
-);
+// await index.upsert(
+//   docs.map((doc, i) => ({
+//     id: `doc-${i}`,
+//     values: doc.embedding,
+//     metadata: {
+//       text: doc.text,
+//     },
+//   })),
+// );
 // console.log(res)
+
+const query = "How is the internship experience?";
+const queryEmbedding = await embeddings.embedQuery(query);
+
+const queryResponse = await index.query({
+  vector: queryEmbedding,
+  topK: 3,
+  includeMetadata: true,
+});
+console.log(JSON.stringify(queryResponse));

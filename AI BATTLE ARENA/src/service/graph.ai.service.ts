7417a092a8b16eb08eaa97f352eb51c40a2f3ce0
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-07-24 16:43:57 +0530

    feat: implement LangGraph state schema for AI battle service and add foundational notes on graph concepts

--- AI BATTLE ARENA/src/service/graph.ai.service.ts
+++ AI BATTLE ARENA/src/service/graph.ai.service.ts
@@ -1,7 +1,7 @@
 import { StateSchema, MessagesValue, StateGraph, START, END } from "@langchain/langgraph";
 
 type JUDGEMENT = {
-    winner: "solution_1" | "solution_2";
+    winner: "solution_1" | "solution_2" | null;
     solution_1_score: number;
     solution_2_score: number;
 }
@@ -11,14 +11,14 @@ type AIBATTLESTATE = {
     solution_1: string;
     solution_2: string;
     judgement: JUDGEMENT;
-}
+};
 
 const state: AIBATTLESTATE = {
     messages: MessagesValue,
     solution_1: "",
     solution_2: "",
     judgement: {
-        winner: "solution_1",
+        winner: null,
         solution_1_score: 0,
         solution_2_score: 0
     }

1f7a8988d475cd8b85a6cc008ef973996d6784e1
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-07-27 01:13:23 +0530

    feat: implement chat API service and initialize React context for state management

---
+++ AI BATTLE ARENA/Frontend/src/features/api/chat.api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+const API = axios.create({
+    baseURL: "/api/",
+    withCredentials: true,
+})
+
+const fetchResponse = async (prompt: string) => {
+    try {
+
+        const response = await API.post("/run-graph", { prompt });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching response:", error);
+    }
+}
+
+export {fetchResponse}

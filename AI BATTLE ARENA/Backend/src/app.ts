88ff1e157c56f0d5d8e383ef9bb4d635dfbcccc5
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-07-27 01:09:14 +0530

    feat: initialize project structure with Vite frontend, Express backend, and API health check endpoint

--- AI BATTLE ARENA/Backend/src/app.ts
+++ AI BATTLE ARENA/Backend/src/app.ts
@@ -7,11 +7,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.send("Server Running 🚀");
 });
 
-app.post("/start", async (req, res) => {
+app.post("/api/run-graph", async (req, res) => {
   const { prompt } = req.body;
   const result = await graph.invoke({ prompt: prompt })
   console.log(result)
--- AI BATTLE ARENA/Frontend/src/app/App.tsx
+++ AI BATTLE ARENA/Frontend/src/app/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import axios from 'axios'
 const App = () => {
+  async function fetchdata(){
+ const res= await axios.get("/api/health")
+  console.log(res)
+  }
+  useEffect(()=>{
+    fetchdata()
+  },[])
   return (
     <div>App</div>
   )

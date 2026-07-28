4c1f3ba280d0adbd8d8defe80c6a51894487adfc
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-07-28 17:44:00 +0530

    feat: initialize project with LangChain dependencies and Gemini model integration

---
+++ LLM APIs/Models/AIMessage.md
@@ -0,0 +1,24 @@
+AIMessage {
+  "content": "I am Gemini, a large language model created by Google. \n\nI'm designed to help answer questions, provide information, assist with writing and problem-solving, and converse on a wide variety of topics. How can I help you today?",
+  "additional_kwargs": {
+    "finishReason": "STOP",
+    "index": 0,
+    "__gemini_function_call_thought_signatures__": {}
+  },
+  "response_metadata": {
+    "tokenUsage": {
+      "promptTokens": 5,
+      "completionTokens": 50,
+      "totalTokens": 202
+    },
+    "finishReason": "STOP",
+    "index": 0
+  },
+  "tool_calls": [],
+  "invalid_tool_calls": [],
+  "usage_metadata": {
+    "input_tokens": 5,
+    "output_tokens": 50,
+    "total_tokens": 202
+  }
+}
--- LLM APIs/Models/main.js
+++ LLM APIs/Models/main.js
@@ -1,23 +1,35 @@
+import "dotenv/config";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatAnthropic } from "@langchain/anthropic";
 import { ChatOpenAI } from "@langchain/openai";
 
-const model1 = new ChatGoogleGenerativeAI({
-  model: "gemini-2.5-flash",
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-flash-latest",
   apiKey: process.env.GEMINI_API_KEY,
 });
 
 
 
-const model2 = new ChatAnthropic({
-model:"claude-sonnet-4",
-apiKey:process.env.ANTHROPIC_API_KEY
-});
+// const model2 = new ChatAnthropic({
+// model:"claude-sonnet-4",
+// apiKey:process.env.ANTHROPIC_API_KEY
+// });
 
 
 
-const model3 = new ChatOpenAI({
-model:"gpt-4.1",
-apiKey:process.env.OPENAI_API_KEY
+// const model3 = new ChatOpenAI({
+// model:"gpt-4.1",
+// apiKey:process.env.OPENAI_API_KEY
 
-});
+// });
+
+// const response = await model.invoke("Who are you?");
+// console.log(response)
+
+const response = await model.invoke([
+  new SystemMessage("You are a Java teacher."),
+  new HumanMessage("Explain OOP."),
+]);
+
+console.log(response.content);

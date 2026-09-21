5335d1724d0df6aefccc994f71b9f17ccbf84b32
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-21 20:14:12 +0530

    Reusable function for reinserting files or document to pinecone

--- Rag/index.js
+++ Rag/index.js
@@ -76,3 +76,78 @@
 
 // const res = await index.upsert({ records: records })
 // console.log(res)
+import { TextLoader } from "@langchain/classic/document_loaders/fs/text";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+const embeddings = new MistralAIEmbeddings({
+  model: "mistral-embed",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+const pc = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY,
+});
+
+const index = pc.index("rag-learning");
+
+
+export const ingestFileonPinecode = async (filePath) => {
+  const extension = path.extname(filePath).toLowerCase();
+
+  let loader;
+
+  if (extension === ".pdf") {
+    loader = new PDFLoader(filePath);
+  } else if (extension === ".txt") {
+    loader = new TextLoader(filePath);
+  } else {
+    throw new Error(`Unsupported file type: ${extension}`);
+  }
+
+  const documents = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 300,
+    chunkOverlap: 50,
+  });
+
+  const chunks = await splitter.splitDocuments(documents);
+
+  const vectors = await embeddings.embedDocuments(
+    chunks.map((chunk) => chunk.pageContent)
+  );
+
+  const records = vectors.map((vector, i) => ({
+    id: `${Date.now()}-${i}`,
+
+    values: vector,
+
+    metadata: {
+      text: chunks[i].pageContent,
+      source: chunks[i].metadata.source,
+      page: chunks[i].metadata?.loc?.pageNumber,
+    },
+  }));
+
+  await index.upsert({
+    records,
+  });
+
+  return {
+    success: true,
+    source: filePath,
+    chunks: chunks.length,
+  };
+};
+
+
+
+// const res = await ingestFileonPinecode("./file.txt")
+// console.log(res)
+

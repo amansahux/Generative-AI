20ced0a680bb2e214c17c5ed76425ae255ab4cd7
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-21 20:32:54 +0530

    Reusable function for reinserting files or document to chroma

--- Rag/index.js
+++ Rag/index.js
@@ -81,6 +81,7 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { MistralAIEmbeddings } from "@langchain/mistralai";
 import { Pinecone } from "@pinecone-database/pinecone";
+import { CloudClient } from "chromadb";
 import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
@@ -94,7 +95,20 @@ const pc = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
 });
 
-const index = pc.index("rag-learning");
+const index = pc.index(process.env.PINECONE_DATABASE);
+
+
+
+const client = new CloudClient({
+  apiKey: process.env.CHROMA_API_KEY,
+  tenant: process.env.CHROMA_TENANT,
+  database: process.env.CHROMA_DATABASE
+});
+const collection = await client.getOrCreateCollection({
+  name: "rag-learning",
+  embeddingFunction: null,
+});
+
 
 
 export const ingestFileonPinecode = async (filePath) => {
@@ -146,8 +160,65 @@ export const ingestFileonPinecode = async (filePath) => {
   };
 };
 
+export const ingestFileonChroma = async (filePath) => {
+  const extension = path.extname(filePath).toLowerCase();
+
+  let loader;
+
+  if (extension === ".pdf") {
+    loader = new PDFLoader(filePath);
+  } else if (extension === ".txt") {
+    loader = new TextLoader(filePath);
+  } else {
+    throw new Error(`Unsupported file type: ${extension}`);
+  }
+
+  const documents = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 300,
+    chunkOverlap: 50,
+  });
+
+  const chunks = await splitter.splitDocuments(documents);
+
+  // 3. Generate embeddings using Mistral
+  const vectors = await embeddings.embedDocuments(
+    chunks.map((chunk) => chunk.pageContent)
+  );
+
+  // 4. Prepare Chroma data
+  const ids = chunks.map((_, i) => `${Date.now()}-${i}`);
+
+  const metadatas = chunks.map((chunk) => ({
+    source: chunk.metadata.source ?? "",
+    page: chunk.metadata?.loc?.pageNumber ?? 0,
+  }));
+
+  const documentsData = chunks.map(
+    (chunk) => chunk.pageContent
+  );
 
 
-// const res = await ingestFileonPinecode("./file.txt")
+  // 5. Store in Chroma
+  await collection.add({
+    ids,
+    embeddings: vectors,
+    metadatas,
+    documents: documentsData,
+  });
+
+  return {
+    success: true,
+    source: filePath,
+    chunks: chunks.length,
+  };
+};
+
+
+// const res = await ingestFileonChroma("./story.pdf")
+// const res = await ingestFileonPinecode("./story.pdf")
+
 // console.log(res)
 
+// ==========================================================================================================
